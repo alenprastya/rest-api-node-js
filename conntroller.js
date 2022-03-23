a848045f1bf46bd812751757280ea91a63a27250
alenprastya <alenprastya@gmail.com>
2022-03-23 20:55:37 +0700

    Menampilkan Semua data Mahasiswa

--- conntroller.js
+++ conntroller.js
@@ -1,8 +1,22 @@
 'use strict'
 
 var response = require('./res');
-var connection = require('./koneksi')
+var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok("A[likasi Rest API Berjalan");
+    response.ok("A[likasi Rest API Berjalan", res);
+};
+
+
+// Menampillan Semua Data Mahasiswa
+
+exports.tampilDataMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
--- koneksi.js
+++ koneksi.js
@@ -7,7 +7,7 @@ const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "rest_api"
+    database: "rest-api"
 })
 
 conn.connect((err) => {
